86a9ac1cbe9b063b664452e22087dc58d311187c
jerrymiahdg <kingbebop15@gmail.com>
2024-03-23 18:27:11 -0700

    responsive

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -24,7 +24,7 @@ const Nav = () => {
       <div className="flex w-full max-w-5xl justify-between gap-10 p-5 items-center">
         <Link to="/" className="font-bold text-4xl flex gap-5 items-center">
           <span class="material-symbols-outlined text-4xl">school</span>
-          HelpYourTeacher.com
+          <a className="hidden sm:block">HelpYourTeacher.com</a>
         </Link>
         <div className="flex gap-10">
           {!ctx.user && (
